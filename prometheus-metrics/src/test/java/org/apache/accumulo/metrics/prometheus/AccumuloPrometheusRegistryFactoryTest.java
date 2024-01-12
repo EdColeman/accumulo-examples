87b77288e93f6e0edaf85fce2860a12c333364a8
Ed Coleman <edcoleman@apache.org>
2024-01-12 00:35:16 +0000

    checkpoint

---
+++ prometheus-metrics/src/test/java/org/apache/accumulo/metrics/prometheus/AccumuloPrometheusRegistryFactoryTest.java
@@ -0,0 +1,10 @@
+package org.apache.accumulo.metrics.prometheus;
+
+import org.junit.jupiter.api.Test;
+
+class AccumuloPrometheusRegistryFactoryTest {
+
+    @Test public void createTest(){
+
+    }
+}
